c350542060ca9a81fc17339397e00e5d86f92e41
Salvador de la Puente González <salva@unoyunodiez.com>
2016-03-30 12:35:48 +0200

    Adding custom headers in case of filesystem. Only works if .htaccess overrides are enabled.

--- class-wp-serve-file.php
+++ class-wp-serve-file.php
@@ -48,12 +48,53 @@ class WP_Serve_File {
 
       $wp_filesystem->mkdir($dir, FS_CHMOD_DIR);
       $wp_filesystem->put_contents($dir . $name, $file['content'], FS_CHMOD_FILE);
+      if (array_key_exists('headers', $file) && is_array($file['headers'])) {
+          $this->set_headers_for_filesystem($dir, $name, $file['headers']);
+      }
     } else {
       set_transient('wpservefile_files_' . $name, $file, YEAR_IN_SECONDS);
     }
 
     return $file;
   }
+
+    private function set_headers_for_filesystem($dir, $filename, $headers) {
+        global $wp_filesystem;
+        $htaccess_file = $dir . '.htaccess';
+        $htaccess_contents = $wp_filesystem->exists($htaccess_file) ?
+                             $wp_filesystem->get_contents($htaccess_file) : '';
+        $header_directives = $this->get_header_directives($headers);
+        $htaccess_contents = $this->update_file_section($htaccess_contents, $filename, $header_directives);
+        $wp_filesystem->put_contents($htaccess_file, $htaccess_contents, FS_CHMOD_FILE);
+    }
+
+    private function get_header_directives($headers) {
+        $header_directives = array();
+        foreach ($headers as $header => $content) {
+            $header_directives[] = "Header set $header $content";
+        }
+        return join("\n", $header_directives);
+    }
+
+    private function update_file_section($current_content, $filename, $directives) {
+        $replacements = 0;
+        $file_section = '<Files "' . $filename . '">';
+        $end_of_file_section = '</Files>';
+        $updated_section = join("\n", array(
+            $file_section,
+            $directives,
+            $end_of_file_section
+        ));
+        $section_regexp = preg_quote($file_section, '/') . '([^<]*)' . preg_quote($end_of_file_section, '/');
+        $section_regexp = "/$section_regexp/";
+        $new_content = preg_replace($section_regexp, $updated_section, $current_content, -1, $replacements);
+
+        // Not found? then simply add the new section
+        if ($replacements === 0) {
+            $new_content = $current_content . "\n" . $updated_section;
+        }
+        return $new_content;
+    }
 
   public function serve_file() {
     $name = $_GET['wpservefile_file'];
